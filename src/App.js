48cbfc4b4cff5b50ced59747f3c019e3aa9a0973
Louis136 <louis.gnolfi@isen.yncrea.fr>
2020-07-02 16:24:17 +0200

    original commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './tdf.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="Onestla">
+          Bienvenue !
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
